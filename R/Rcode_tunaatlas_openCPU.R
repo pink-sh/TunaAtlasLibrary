6982562b691f7cac541eae6fa98f12a1f66a1b82
Enrico Anello <enrico.anello@gmail.com>
2016-06-08 14:28:42 +0200

    Update Rcode_tunaatlas_openCPU.R

--- R/Rcode_tunaatlas_openCPU.R
+++ R/Rcode_tunaatlas_openCPU.R
@@ -72,3 +72,52 @@ plotQuantitiesInTonnesByCountry <- function(species=c(), start=1946, end=2014, c
   m1$save('output.html', standalone = TRUE)
   return (toJSON(aggr04))
 }
+
+getSpecies <- function() {
+  library(jsonlite)
+  library(plyr)
+  library(RCurl)
+  library (DBI)
+  library ("RPostgreSQL")
+
+  query <- "SELECT
+  english_name_ocean as ASD,
+  year as seasonYear,
+  month as SeasonMonthNr,
+  month || ' - ' || month_name as SeasonMonth,
+  month_name as MonthNm,
+  codesource_gear as GearCode,
+  english_name_gear as Gear,
+  species.codesource_species as SpeciesCode,
+  scientific_name as ScientificName,
+
+  family as ScientificFamilyName,
+  v_catch as CatchWeight,
+  scientific_name || ' - ' || species.codesource_species as Species,
+  species.codesource_species as TargetSpeciesCode,
+  codesource_flag as CountryCode,
+  english_name_flag as Country
+
+  from tunaatlas.total_catches tc
+
+  INNER JOIN area.area USING (id_area)
+  INNER JOIN time.time USING (id_time)
+  INNER JOIN gear.gear_labels ON (tc.id_geargroup_standard=gear_labels.id_gear)
+  INNER JOIN flag.flag_labels ON (tc.id_flag_standard=flag_labels.id_flag)
+  INNER JOIN species.species_labels ON (tc.id_species_standard=species_labels.id_species)
+  INNER JOIN species.species ON (tc.id_species_standard=species.id_species)
+  INNER JOIN species.species_asfis ON (species.codesource_species=species_asfis.x3a_code)
+  INNER JOIN area.rfmos_convention_areas_fao ON (rfmos_convention_areas_fao.id_origin_institution=tc.id_ocean)
+  WHERE year<=2014 AND id_catchunit IN (1,3)"
+
+  drv <- dbDriver("PostgreSQL")
+  con_sardara <- dbConnect(drv, user = "invsardara",password="fle087",dbname="sardara_world",host ="db-tuna.d4science.org",port=5432)
+  tuna <- dbGetQuery(con_sardara, query)
+  colnames(tuna)<-c("ASD","SeasonYear","SeasonMonthNr","SeasonMonth","MonthNm","GearCode","Gear","TargetSpeciesCode","ScientificName","ScientificFamilyName","CatchWeightT","Species","SpeciesCode","CountryCode","Country")
+
+  myData <- tuna
+  plyed <- ddply(myData, c("ScientificName","SpeciesCode"), head, 1)
+  ret <- data.frame("id" = plyed$SpeciesCode, "name" = plyed$ScientificName)
+  ret <- ret[!apply(ret, 1, function(x) any(x=="")),]
+  return (toJSON(ret))
+}
