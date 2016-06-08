9a137583b068fde5ce1544864ff3fb54f717639f
Enrico Anello <enrico.anello@gmail.com>
2016-06-08 14:36:16 +0200

    Update Rcode_tunaatlas_openCPU.R

--- R/Rcode_tunaatlas_openCPU.R
+++ R/Rcode_tunaatlas_openCPU.R
@@ -121,3 +121,139 @@ getSpecies <- function() {
   ret <- ret[!apply(ret, 1, function(x) any(x=="")),]
   return (toJSON(ret))
 }
+
+plotQuantitiesInTonnes <- function(species=c(), start=1946, end=2016, chart="Bar") {
+  library (DBI)
+  library ("RPostgreSQL")
+  library(rCharts)
+  library(dplyr)
+  library(jsonlite)
+
+  speciesList <- ""
+  for (sp in species) {
+    speciesList <- paste0(speciesList, "'", sp, "'", ',')
+  }
+  speciesList <- substr(speciesList, 1, nchar(speciesList)-1)
+
+  whereConditions <- ""
+  whereConditions <- paste0(whereConditions," year <= ", end, " ")
+  whereConditions <- paste0(whereConditions," AND ", " year >= ", start, " ")
+
+  if (speciesList != "") {
+    whereConditions <- paste0(whereConditions," AND ", " species.codesource_species IN (", speciesList, ") ")
+  }
+  whereConditions <- paste0(whereConditions," AND ", " id_catchunit IN (1,3) ")
+
+  query <- "SELECT
+  english_name_ocean as ASD,
+  year as seasonYear,
+  month as SeasonMonthNr,
+  month || ' - ' || month_name as SeasonMonth,
+  month_name as MonthNm,
+  codesource_gear as GearCode,
+  english_name_gear as Gear,
+  species.codesource_species as SpeciesCode,
+  scientific_name as ScientificName,
+
+  family as ScientificFamilyName,
+  v_catch as CatchWeight,
+  scientific_name || ' - ' || species.codesource_species as Species,
+  species.codesource_species as TargetSpeciesCode,
+  codesource_flag as CountryCode,
+  english_name_flag as Country
+
+  from tunaatlas.total_catches tc
+
+  INNER JOIN area.area USING (id_area)
+  INNER JOIN time.time USING (id_time)
+  INNER JOIN gear.gear_labels ON (tc.id_geargroup_standard=gear_labels.id_gear)
+  INNER JOIN flag.flag_labels ON (tc.id_flag_standard=flag_labels.id_flag)
+  INNER JOIN species.species_labels ON (tc.id_species_standard=species_labels.id_species)
+  INNER JOIN species.species ON (tc.id_species_standard=species.id_species)
+  INNER JOIN species.species_asfis ON (species.codesource_species=species_asfis.x3a_code)
+  INNER JOIN area.rfmos_convention_areas_fao ON (rfmos_convention_areas_fao.id_origin_institution=tc.id_ocean)
+  WHERE "
+
+  query <- paste0(query, whereConditions)
+
+  drv <- dbDriver("PostgreSQL")
+  con_sardara <- dbConnect(drv, user = "invsardara",password="fle087",dbname="sardara_world",host ="db-tuna.d4science.org",port=5432)
+
+  tuna <- dbGetQuery(con_sardara, query)
+
+  print (tuna)
+
+  colnames(tuna)<-c("ASD","SeasonYear","SeasonMonthNr","SeasonMonth","MonthNm","GearCode","Gear","TargetSpeciesCode","ScientificName","ScientificFamilyName","CatchWeightT","Species","SpeciesCode","CountryCode","Country")
+
+  #myCsv <- getURL(file)
+  #myData <- read.csv(textConnection(myCsv))
+  myData <- tuna
+
+  if (length(species > 0)) {
+    reduced <- filter(myData, SpeciesCode %in% species)
+  } else {
+    reduced <- myData
+    species <- unique(reduced$SpeciesCode)
+  }
+  OUT <- data.frame("SeasonYear"=(start:end))
+  for (sp in species) {
+    aggr0 <- filter(reduced, SpeciesCode == sp)
+    aggr01 <- aggregate(aggr0$CatchWeightT, by=list(SeasonYear=aggr0$SeasonYear, ScientificName=aggr0$ScientificName), FUN=sum)
+    aggr02 <- transform(aggr01, SeasonYear = as.character(SeasonYear), CatchWeightT = as.numeric(x))
+    aggr03 <- filter(aggr02, SeasonYear >= start, SeasonYear <= end)
+    ifelse(lengths(aggr03, use.names = TRUE) == 0,next,1)
+    aggr03$x <- NULL
+
+
+    vector <- c()
+    i = 1;
+    scientificName <- ""
+    apply(OUT, 1, function(row1) {
+      yrOut = row1['SeasonYear']
+      value <- 0
+      apply(aggr03, 1, function(row2) {
+        yrIn = row2['SeasonYear']
+
+        scientificName <<- row2['ScientificName']
+        if (!is.na(yrIn)) {
+          if (yrOut == yrIn) {
+            value <<- row2['CatchWeightT']
+          }
+        }
+      })
+      vector <<- c(vector, value)
+      i <<- i + 1
+    })
+    OUT[[scientificName]] <- vector
+  }
+  OUT <- transform(OUT, SeasonYear = as.character(SeasonYear))
+  namesOut <- c()
+  for (name in names(OUT)) {
+    if (name != 'SeasonYear') {
+      OUT[, name] = as.double(OUT[, name])
+      namesOut <- c(namesOut, name)
+    }
+  }
+  m1 <- mPlot(x = "SeasonYear", y = namesOut, type = chart, data = OUT, stacked = "TRUE")
+  m1$set(hoverCallback = "#! function(index, options, content) {
+         var row = options.data[index];
+         var tuples = [];
+         var YEAR = '';
+         for (var key in row) {
+         if (key != 'SeasonYear') {
+         tuples.push([key, parseInt(row[key])]);
+         } else {
+         year = row[key]
+         }
+         }
+         tuples.sort(function(a, b) {return b[1] - a[1]});
+         var ret = [];
+         ret.push('<div style=\"color: red;\">' + year + '</div>');
+         for (var i = 0; i < tuples.length; i++) {
+         if (i > 10) break;
+         ret.push(tuples[i][0].replace('.', ' ') + ':' + tuples[i][1]);
+         }
+         return ret.join('<br />'); } !#")
+  m1$save('output.html', standalone = TRUE)
+  return (toJSON(OUT))
+}
