586011848649a9581209cd94cf3827b07db67cfb
Enrico Anello <enrico.anello@gmail.com>
2016-07-18 12:59:47 +0200

    Update Rcode_tunaatlas_openCPU.R

--- R/Rcode_tunaatlas_openCPU.R
+++ R/Rcode_tunaatlas_openCPU.R
@@ -79,7 +79,6 @@ getSpecies <- function() {
   library(RCurl)
   library (DBI)
   library ("RPostgreSQL")
-
   query <- "SELECT
   english_name_ocean as ASD,
   year as seasonYear,
@@ -90,31 +89,29 @@ getSpecies <- function() {
   english_name_gear as Gear,
   species.codesource_species as SpeciesCode,
   scientific_name as ScientificName,
-
+  
   family as ScientificFamilyName,
   v_catch as CatchWeight,
   scientific_name || ' - ' || species.codesource_species as Species,
   species.codesource_species as TargetSpeciesCode,
   codesource_flag as CountryCode,
   english_name_flag as Country
-
-  from tunaatlas.catches tc
-
+  
+  from tunaatlas.total_catches tc
+  
   INNER JOIN area.area USING (id_area)
   INNER JOIN time.time USING (id_time)
-  INNER JOIN gear.gear_labels ON (tc.id_geargroup_standard=gear_labels.id_gear)
+  INNER JOIN gear.gear_labels ON (tc.id_geargroup_tunaatlas=gear_labels.id_gear)
   INNER JOIN flag.flag_labels ON (tc.id_flag_standard=flag_labels.id_flag)
   INNER JOIN species.species_labels ON (tc.id_species_standard=species_labels.id_species)
   INNER JOIN species.species ON (tc.id_species_standard=species.id_species)
   INNER JOIN species.species_asfis ON (species.codesource_species=species_asfis.x3a_code)
   INNER JOIN area.rfmos_convention_areas_fao ON (rfmos_convention_areas_fao.id_origin_institution=tc.id_ocean)
   WHERE year<=2014 AND id_catchunit IN (1,3) AND v_catch > 0"
-
   drv <- dbDriver("PostgreSQL")
   con_sardara <- dbConnect(drv, user = "invsardara",password="fle087",dbname="sardara_world",host ="db-tuna.d4science.org",port=5432)
   tuna <- dbGetQuery(con_sardara, query)
   colnames(tuna)<-c("ASD","SeasonYear","SeasonMonthNr","SeasonMonth","MonthNm","GearCode","Gear","TargetSpeciesCode","ScientificName","ScientificFamilyName","CatchWeightT","Species","SpeciesCode","CountryCode","Country")
-
   myData <- tuna
   plyed <- ddply(myData, c("ScientificName","SpeciesCode"), head, 1)
   ret <- data.frame("id" = plyed$SpeciesCode, "name" = plyed$ScientificName)
